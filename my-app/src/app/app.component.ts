caae8d70d58efa2cf76651f1acf8bae74b9d1341
森田賢二 <kenjimoritata@gmail.com>
2017-02-11 15:27:15 +0900

    add sample

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!!';
   value = 0;
+  values = [12, 44, 99, 10];
+  valueif = true;
+  valueswitch = 'case1';
+  valuepipe = 'aBc';
+  isActive = true;
+  myClasses = {
+    largeText: true,
+    infoColor: true
+  }
+
+  save(event){
+    console.log(event);
+  }
 }
